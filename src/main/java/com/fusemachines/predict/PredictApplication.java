b19065c4cbf2c4ef818b003a17f0536208325793
rokinfuse <rokin@fusemachines.com>
2018-06-15 15:01:05 +0545

    Enable scheduling

--- src/main/java/com/fusemachines/predict/PredictApplication.java
+++ src/main/java/com/fusemachines/predict/PredictApplication.java
@@ -2,11 +2,13 @@ package com.fusemachines.predict;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableScheduling
 public class PredictApplication {
 
 	public static void main(String[] args) {
