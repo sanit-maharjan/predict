1095501beb4bf11565439ee37965473acc5f733c
Sanit Maharjan <sanit@fusemachines.com>
2018-06-15 12:30:43 +0545

    property

--- src/main/java/com/fusemachines/predict/game/GameController.java
+++ src/main/java/com/fusemachines/predict/game/GameController.java
@@ -3,6 +3,7 @@ package com.fusemachines.predict.game;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
